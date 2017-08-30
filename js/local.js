e69f5a08a3dc573fdc2d689eb9956e416b30b199
wojiaxiaoyueyue <qz274268382@gmail.com>
2017-08-30 16:55:30 +0800

    Add some functions
    Gameover, time, score, checkClear and change some codes style.

--- js/local.js
+++ js/local.js
@@ -1,6 +1,16 @@
 var Local = function(){
 	//游戏对象
 	var game;
+
+	//时间间隔
+	const INTERVAL = 200;
+	//定时器
+	var timer = null;
+
+	//时间计数器
+	var timeCount = 0;
+	//时间
+	var time = 0;
 	// 绑定键盘事件
 	var bindKeyEvent = function(){
 		document.onkeydown = function(e){
@@ -22,15 +32,70 @@ var Local = function(){
 			}
 		}
 	}
+	//移动
+	var move = function(){
+		timeFunc();
+		if(!game.down()){
+			game.fixed();
+			var line = game.checkClear();
+			if(line){
+				game.addScore(line);
+			}
+			var gameOver = game.checkGameover();
+			if(gameOver){
+				game.gameover(false);
+				stop();
+			}
+			else{
+			game.performNext(generateType(), generateDir());
+			}
+		}
+		
+	}
+
+	//计时函数
+	var timeFunc = function(){
+		timeCount = timeCount + 1;
+		if(timeCount == 5){
+			timeCount = 0;
+			time = time + 1;
+			game.setTime(time);
+		}
+	}
+
+	//随机生成方块种类
+	var generateType = function(){
+		return Math.ceil(Math.random() * 7) - 1;
+	}
+
+	//随机生成方块旋转次数
+	var generateDir = function(){
+		return Math.ceil(Math.random() * 4) - 1;
+	}
+
 	// 开始
 	var start = function(){
 		var doms = {
 			gameDiv: document.getElementById('game'),
-			nextDiv: document.getElementById('next')
+			nextDiv: document.getElementById('next'),
+			timeDiv: document.getElementById('time'),
+			scoreDiv: document.getElementById('score'),
+			resultDiv: document.getElementById('gameover')
 		}
 		game = new Game();
-		game.init(doms);
+		game.init(doms, generateType(), generateDir());
 		bindKeyEvent();
+		game.performNext(generateType(), generateDir());
+		timer = setInterval(move, INTERVAL)
+	}
+
+	//结束
+	var stop = function(){
+		if(timer){
+			clearInterval(timer);
+			timer = null;
+		}
+		document.onkeydown = null;
 	}
 
 	//导出api
